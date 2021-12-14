7be6c8deb0e369b5f4c555ad83c8380cc25d3098
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-12-14 19:41:09 +0200

    StyleSheet

---
+++ src/assets/index.ts
@@ -0,0 +1,20 @@
+let LOGO = require('./Images/logo.png');
+let INITIAL_BACKGROUND = require('./Images/1.jpg');
+let SIGNUP_BACKGROUND = require('./Images/2.jpg');
+let SIGNIN_BACKGROUND = require('./Images/3.jpg');
+let NAME_BACKGROUND = require('./Images/4.jpg');
+let DATA_BACKGROUND = require('./Images/5.jpg');
+let WORKOUT_BACKGROUND = require('./Images/6.jpg');
+let SUCCESS_BACKGROUND = require('./Images/7.jpg');
+
+export {
+  LOGO,
+  INITIAL_BACKGROUND,
+  SIGNUP_BACKGROUND,
+  SIGNIN_BACKGROUND,
+  NAME_BACKGROUND,
+  DATA_BACKGROUND,
+  WORKOUT_BACKGROUND,
+  SUCCESS_BACKGROUND,
+};
+
